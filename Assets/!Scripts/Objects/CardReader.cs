4cd057440ba0d44789f7dd9ca0416379ddd1195a
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2022-10-02 12:21:45 +0200

    Added sounds to card reader and pickup sounds.

--- Assets/!Scripts/Objects/CardReader.cs
+++ Assets/!Scripts/Objects/CardReader.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 [RequireComponent(typeof(InteractableObject))]
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]
 class CardReader : MonoBehaviour
 {
 	Animator animator = null;
+	AudioSource audioSource = null;
+
+	[SerializeField]
+	AudioClip _valid;
+	[SerializeField]
+	AudioClip _invalid;
 
 	protected virtual void Awake()
 	{
 		InteractableObject interactable = GetComponent<InteractableObject>();
 		interactable.OnInteracted += InteractHandler;
 		animator = GetComponent<Animator>();
+		audioSource = GetComponent<AudioSource>();
 	}
 
 	protected virtual void InteractHandler(InteractableObject interactWith)
@@ -31,11 +39,15 @@ class CardReader : MonoBehaviour
 	{
 		Debug.Log("Card is valid");
 		animator.SetTrigger("Valid");
+		audioSource.clip = _valid;
+		audioSource.Play();
 	}
 
 	protected virtual void BeepNegative()
 	{
 		Debug.Log("Card is invalid");
 		animator.SetTrigger("Invalid");
+		audioSource.clip = _invalid;
+		audioSource.Play();
 	}
 }
