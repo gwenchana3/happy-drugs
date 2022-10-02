0e5a84c1e134c72acb223cfddf40165d3d111bd3
Reispfannenfresser <34158734+Reispfannenfresser@users.noreply.github.com>
2022-10-02 15:00:51 +0200

    Added customer interaction with InteractableObjects

--- Assets/!Scripts/Spawner/CustomerManager.cs
+++ Assets/!Scripts/Spawner/CustomerManager.cs
@@ -4,90 +4,111 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.VFX;
 
+[RequireComponent(typeof(InteractableObject))]
 public class CustomerManager : MonoBehaviour
 {
-    public DrugType DemandedDrug;
-    public float WaitingTime;
+	public DrugType DemandedDrug;
+	public float WaitingTime;
 
-    private float _waitedTime;
-    [HideInInspector]
-    public DestinationPoint DestinationPoint;
-    private NavMeshAgent _navAgent;
-    private ParticleSystem _particleSystem;
-    private VisualEffect _visualEffect;
+	private float _waitedTime;
+	[HideInInspector]
+	public DestinationPoint DestinationPoint;
+	private NavMeshAgent _navAgent;
+	private ParticleSystem _particleSystem;
+	private VisualEffect _visualEffect;
 
-    private SpawnController _spawnController;
+	private SpawnController _spawnController;
 
-    private void Awake()
-    {
-        _navAgent = GetComponentInChildren<NavMeshAgent>();
-        _visualEffect = GetComponent<VisualEffect>();
-        _particleSystem = GetComponent<ParticleSystem>();
-    }
+	private void Awake()
+	{
+		_navAgent = GetComponentInChildren<NavMeshAgent>();
+		_visualEffect = GetComponent<VisualEffect>();
+		_particleSystem = GetComponent<ParticleSystem>();
+		InteractableObject interactable = GetComponent<InteractableObject>();
+		interactable.OnInteracted += InteractHandler;
+	}
 
-    private void Start()
-    {
-        _waitedTime = Time.time;
-        _spawnController = Manager.Use<PlayManager>().Spawner;
-        EnterRoom();
-    }
+	private void Start()
+	{
+		_waitedTime = Time.time;
+		_spawnController = Manager.Use<PlayManager>().Spawner;
+		EnterRoom();
+	}
 
-    private void Update()
-    {
-        if(!_navAgent.hasPath)
-        {
-            Vector3 dir = Camera.main.transform.position - transform.position;
-            dir.y = 0;
-            transform.rotation = Quaternion.LookRotation(dir);
-        }
+	private void Update()
+	{
+		if (!_navAgent.hasPath)
+		{
+			Vector3 dir = Camera.main.transform.position - transform.position;
+			dir.y = 0;
+			transform.rotation = Quaternion.LookRotation(dir);
+		}
 
-        if (_waitedTime + WaitingTime <= Time.time)
-        {
-            CommitSuicide();
-        }
-    }
+		if (_waitedTime + WaitingTime <= Time.time)
+		{
+			CommitSuicide();
+		}
+	}
 
+	private void InteractHandler(InteractableObject interactWith)
+	{
+		Drug drug = interactWith.GetComponent<Drug>();
+		if (drug != null)
+		{
+			ReceiveDrug(drug);
+			return;
+		}
 
-    public void ReceiveDrug(Drug drug)
-    {
-        if(drug.drugType != DemandedDrug)
-        {
-            CommitSuicide();
-        }
+		Stamp stamp = interactWith.GetComponent<Stamp>();
+		if (stamp != null)
+		{
+			CommitSuicide();
+			stamp.has_ink = false;
+			Debug.Log("Used stamp on customer");
+		}
+	}
 
-        else
-        {
-            LeaveRoom();
-        }
-    }
+	public void ReceiveDrug(Drug drug)
+	{
+		if (drug.drugType != DemandedDrug)
+		{
+			CommitSuicide();
+		}
+		else
+		{
+			LeaveRoom();
+		}
 
-    public void EnterRoom()
-    {
-        _navAgent.SetDestination(DestinationPoint.transform.position);
-        DestinationPoint.IsOccupied = true;
-    }
+		Destroy(drug.gameObject);
+	}
 
-    public void LeaveRoom()
-    {
-        DestinationPoint.IsOccupied = false;
-        StartCoroutine(PlayParticles());
-        Destroy(gameObject);
-    }
+	public void EnterRoom()
+	{
+		_navAgent.SetDestination(DestinationPoint.transform.position);
+		DestinationPoint.IsOccupied = true;
+	}
 
-    IEnumerator PlayParticles()
-    {
-        _particleSystem.Play();
-        yield return new WaitForSeconds(1.5f);
-    }
+	public void LeaveRoom()
+	{
+		DestinationPoint.IsOccupied = false;
+		StartCoroutine(PlayParticles());
+		Destroy(gameObject);
+	}
 
-    IEnumerator PlayVisualEffect()
-    {
-        _visualEffect.Play();
-        yield return new WaitForSeconds(1.5f);
-    }
-    
-    public void CommitSuicide()
-    {
-        // TODO: Go down the Suisilde
-    }
+	IEnumerator PlayParticles()
+	{
+		_particleSystem.Play();
+		yield return new WaitForSeconds(1.5f);
+	}
+
+	IEnumerator PlayVisualEffect()
+	{
+		_visualEffect.Play();
+		yield return new WaitForSeconds(1.5f);
+	}
+
+	public void CommitSuicide()
+	{
+		// TODO: Go down the Suisilde
+	}
 }
